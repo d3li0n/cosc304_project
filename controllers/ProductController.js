b56ecae81e7e1761ae48acdd29139e072fb03ee7
Dima Zhuravel <dmitrozhyravel@gmail.com>
2021-08-10 13:04:12 -0700

    fixed product page, clean up

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,28 +1,63 @@
 const sql = require('mssql');
 const db = require('../dbconfig');
-const { addProduct } = require('./CartController');
+const moment = require('moment');
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
 
-    async displayProduct(req, res) {
-        let productId = req.query.id;
-        let products = req.session.productList;
-        await sql.connect(db.sqlConfig).then(pool => {
-            return pool.request()
-   
-                    .input('pid', sql.Int, productId)
-                    .query(`SELECT reviewRating, enteredName, reviewComment, CONVERT(varchar, reviewDate, 23) as date FROM review WHERE productId = @pid`);
+	async displayProduct(req, res) {
+		const prodId = parseInt(req.params.id);
+		if (prodId < 1) {
+			return res.status(301).redirect('/store');
+		}
+		let reviewsList = {};
+		let product = {};
+		await sql.connect(db.sqlConfig).then(pool => {
+			return pool.request()
+					.input('pid', sql.Int, prodId)
+					.query(`SELECT cast(productImage as nvarchar(max)) as image, productName, productDesc, productPrice, productImageURL, categoryName from product join category on category.categoryId = product.categoryId where productId = @pid`);
+		}).then(result => {
+				if (result.rowsAffected[0] === 0) {
+					return res.status(301).redirect('/store');
+				} else {
+						const file = fs.readFileSync(path.join(__dirname, `../public/images/store_images/${result.recordset[0].image}`));
+						const base64String = Buffer.from(file).toString('base64');
+						product = {
+								productId: req.params.id,
+								productTitle: `${result.recordset[0].productName}`,
+								productCategory: `${result.recordset[0].categoryName}`,
+								productDescription: `${result.recordset[0].productDesc}`,
+								productPrice: (result.recordset[0].productPrice).toFixed(2),
+								imageUrl: `${result.recordset[0].productImageURL}`,
+								image: `${base64String}`,
+						};
+				}
+		}).catch(err => {
+			res.status(403).send({ data: { status: 403, message: "Error: Invalid statement."}});
+		});
+		await sql.connect(db.sqlConfig).then(pool => {
+			return pool.request()
+				.input('pid', sql.Int, prodId)
+				.query(`SELECT firstName, lastName, reviewRating, reviewComment, reviewDate, reviewId FROM review join customer on customer.customerId=review.customerId WHERE productId = @pid`);
+		}).then(result => {
+			if (result.rowsAffected[0] !== 0) {
+				for(var i =0; i < result.rowsAffected[0]; i++) {
+					reviewsList[result.recordsets[0][i].reviewId] = {
+						customerName: `${result.recordsets[0][i].firstName} ${result.recordsets[0][i].lastName}`,
+						dateReview: moment(result.recordsets[0][i].reviewDate).format('YYYY-MM-DD'),
+						review: `${result.recordsets[0][i].reviewComment}`,
+						stars: []
+					}
+					for (var x = 0; x < result.recordsets[0][i].reviewRating; x++)
+						(reviewsList[result.recordsets[0][i].reviewId].stars).push(x);
+				}
+			}
 
-                }).then(result => {
-
-                    const prodName = result.recordset[0].productName;
-                    const prodPrice = result.recordset[0].productPrice;
-                    if (products[prodId] === undefined)
-                        products[prodId] = {title: prodName, price: (prodPrice).toFixed(2), quantity: 1, totalPrice: (prodPrice).toFixed(2)};
-                    else {
-                        products[prodId].quantity = products[prodId].quantity + 1;
-                        products[prodId].totalPrice = (parseFloat(products[prodId].totalPrice) + prodPrice).toFixed(2);
-                   } }).catch(err => {
-                        res.status(403).send({ data: { status: 403, message: "Error: Invalid Product id."}});
-                    })  
-}
+			return res.status(200).render('productPage', { title: 'Product', reviews: reviewsList, product: product, isEmpty: (Object.keys(reviewsList).length === 0) });
+		}).catch(err => {
+			console.log(err);
+			res.status(403).send({ data: { status: 403, message: "Error: Invalid Product id."}});
+		});
+	}
 }
--- routes.js
+++ routes.js
@@ -5,40 +5,29 @@ const userController = require('./controllers/UserController');
 const storeController = require('./controllers/StoreController');
 const adminController = require('./controllers/AdminController');
 const productController = require('./controllers/ProductController');
-router.get('/', (req, res) => {
-	res.status(200).render('index', { title: 'Home' });
-});
+
+router.get('/', (req, res) => { return res.status(200).render('index', { title: 'Home' }); });
 
 router.get('/store', storeController.getProducts);
 
-
 router.get('/cart', (req, res) => {
-	// showcart.js
-	let t = 0;
-	let shipTotal = 0;
-	let subTotal = 0;
+	let t = 0, shipTotal = 0, subTotal = 0;
 
 	if (req.session.productList !== undefined && Object.keys(req.session.productList).length !== 0) {
-		Object.keys(req.session.productList).forEach(key => {
-			t += parseFloat(req.session.productList[key].totalPrice);
-		});	
-		shipTotal = (t*0.10);
-		subTotal = (t+shipTotal);
-		t = t.toFixed(2);
+		Object.keys(req.session.productList).forEach(key => { t += parseFloat(req.session.productList[key].totalPrice); });	
+		shipTotal = (t*0.10), subTotal = (t+shipTotal), t = t.toFixed(2);
 		shipTotal = shipTotal.toFixed(2);
 		subTotal = subTotal.toFixed(2);
 	}
-
-	let totalArray = {total:t, shipTotal: shipTotal, subTotal: subTotal};
-	
-	res.status(200).render('cart', { title: 'My Cart', isCart: (req.session.productList !== undefined && Object.keys(req.session.productList).length !== 0) ? true : false , tArray: totalArray});
+	const totalArray = {total: t, shipTotal: shipTotal, subTotal: subTotal};
+	return res.status(200).render('cart', { title: 'My Cart', isCart: (req.session.productList !== undefined && Object.keys(req.session.productList).length !== 0) ? true : false , tArray: totalArray});
 });
 
 router.get('/cart/checkout', cartController.cartCheckout);
 
 router.post('/product/:id/addCart', cartController.addProduct);
 
-router.get('/product/:id', productController.getProductInfo);
+router.get('/product/:id', productController.displayProduct);
 
 router.get('/account', userController.validateApiToken, userController.getUser);
 
@@ -57,30 +46,19 @@ router.post('/restore', userController.restoreCreate);
 router.get('/restore/:token', userController.restoreLoadForm);
 router.put('/restore/:token', userController.restoreConfirm);
 
-router.get('/login', (req, res) => {
-	if (req.session.API_TOKEN === undefined) {
-		res.status(200).render('loginPage', { title: 'Login' }); 
-	} else {
-		res.status(301).redirect('/');
-	}
-});
+router.get('/login', (req, res) => { return (req.session.API_TOKEN === undefined) ? res.status(200).render('loginPage', { title: 'Login' }) : res.status(301).redirect('/'); });
 
 router.post('/login', userController.authUser, userController.fetchCart);
 
 router.post('/logout', (req, res) => {
-	if (req.session.API_TOKEN === undefined) {
-		res.status(403).send({ data: { code: 403, message: "Error: Not Authorized" }});
-	} else {
-		req.session.destroy();
-		res.status(200).send({ data: { code: 200, message: "Success." }});
-	}
+	if (req.session.API_TOKEN === undefined) return res.status(403).send({ data: { code: 403, message: "Error: Not Authorized" }});
+	req.session.destroy();
+	return res.status(200).send({ data: { code: 200, message: "Success." }});
 })
 
 router.use('/admin/*', adminController.auth);
 
-router.get('/admin', adminController.auth, (req, res) => {
-	res.status(200).render('admin', { title: 'Admin Portal' });
-});
+router.get('/admin', adminController.auth, (req, res) => { return res.status(200).render('admin', { title: 'Admin Portal' }); });
 
 router.post('/a/login', adminController.validate);
 
@@ -98,11 +76,6 @@ router.post('/admin/ship/:id', adminController.ship);
 
 router.get('/admin/orders', adminController.loadOrders );
 
-router.get('/admin/products', (req, res) => {
-});
-
-router.get('*', (req, res) => {
-	res.status(404).render('error', { title: 'Page Not Found' });
-});
+router.get('*', (req, res) => { res.status(404).render('error', { title: 'Page Not Found' }); });
 
 module.exports = router;
